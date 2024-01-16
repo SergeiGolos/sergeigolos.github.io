c867c43c054fb4c8c0bb23436a1464f17208b340
Sergei Golos <sergei.golos@gmail.com>
2024-01-15 19:39:07 -0500

    added icons and images processing

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,22 +1,28 @@
+import { FaGithub, FaStackOverflow } from "@qwikest/icons/font-awesome"
 import { Link } from "@builder.io/qwik-city"
-
 export function Intro() {
   return (
     <>      
-      <img class="mx-auto h-64 w-64 rounded-full" src="https://avatars.githubusercontent.com/u/660992" />              
+      <img 
+        height={256}
+        width={256}
+        class="mx-auto h-64 w-64 rounded-full" 
+        src="https://avatars.githubusercontent.com/u/660992"        
+      />              
       <h1 class="mt-14 font-display text-4xl/tight font-light text-white">
         Sergei Golos{' '}<span class="text-sky-300">software enthusiast</span> and <span class="text-sky-300">forever student</span>
       </h1>
-      <div class="mt-8 flex flex-wrap justify-center gap-x-1 gap-y-3 sm:gap-x-2 lg:justify-start">
-        {/* <IconLink href="#" icon={BookIcon} className="flex-none">
-          Documentation
-        </IconLink>
-        <IconLink href="#" icon={GitHubIcon} className="flex-none">
-          GitHub
-        </IconLink>
-        <IconLink href="/feed.xml" icon={FeedIcon} className="flex-none">
-          RSS
-        </IconLink> */}
+      <div class="mt-8 flex justify-center sm:gap-x-2 lg:justify-start">      
+        <Link href="https://github.com/sergeigolos/">
+          <span class="flex gap-x-2">
+            <FaGithub/>
+            sergeigolos</span>
+        </Link>
+        <Link href="https://stackoverflow.com/users/190188/sergei-golos">
+          <span class="flex gap-x-2">
+            <FaStackOverflow />
+            sergeigolos</span>
+        </Link>              
       </div>
     </>
   )
