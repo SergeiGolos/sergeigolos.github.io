7064361c85b5547e20cc2599d955bb2b9131db09
Sergei Golos <sergei.golos@gmail.com>
2024-01-21 11:57:46 -0500

    re-order

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,19 +1,19 @@
-import { FaGithub, FaStackOverflow, FaLinkedin , FaBlogSolid} from "@qwikest/icons/font-awesome"
+import { FaGithub, FaStackOverflow, FaLinkedin, FaBlogSolid } from "@qwikest/icons/font-awesome"
 import { Link } from "@builder.io/qwik-city"
 export function Intro() {
   return (
-    <>      
-      <img 
+    <>
+      <img
         height={256}
         width={256}
-        class="mx-auto h-64 w-64 rounded-full" 
-        src="https://avatars.githubusercontent.com/u/660992"        
-      />              
+        class="mx-auto h-64 w-64 rounded-full"
+        src="https://avatars.githubusercontent.com/u/660992"
+      />
       <h1 class="mt-14 font-display text-4xl/tight font-light text-white">
         Sergei Golos{' '}<span class="text-sky-300">software enthusiast</span> and <span class="text-sky-300">forever student</span>
       </h1>
-      <div class="mt-8 flex justify-center sm:gap-x-2 lg:justify-start">      
-                      
+      <div class="mt-8 flex justify-center sm:gap-x-2 lg:justify-start">
+
       </div>
     </>
   )
@@ -22,26 +22,26 @@ export function Intro() {
 export function IntroFooter() {
   return (
     <p class="flex items-center gap-x-2 text-[0.8125rem]/6 text-gray-500">
+      <Link href="https://bitcobblers.com/">
+        <span class="flex gap-x-2">
+          <FaBlogSolid font-size={24} />
+        </span>
+      </Link>
       <Link href="https://github.com/sergeigolos/">
-          <span class="flex gap-x-2">
-            <FaGithub font-size={24}/>
-          </span>
-        </Link>
-        <Link href="https://stackoverflow.com/users/190188/sergei-golos">
-          <span class="flex gap-x-2">
-            <FaStackOverflow font-size={24}/>
-          </span>
-        </Link>
-        <Link href="https://www.linkedin.com/in/sergei-golos-11172164/">
-          <span class="flex gap-x-2">
-            <FaLinkedin font-size={24}/>
+        <span class="flex gap-x-2">
+          <FaGithub font-size={24} />
+        </span>
+      </Link>
+      <Link href="https://stackoverflow.com/users/190188/sergei-golos">
+        <span class="flex gap-x-2">
+          <FaStackOverflow font-size={24} />
+        </span>
+      </Link>
+      <Link href="https://www.linkedin.com/in/sergei-golos-11172164/">
+        <span class="flex gap-x-2">
+          <FaLinkedin font-size={24} />
           <s></s></span>
-        </Link>
-        <Link href="https://bitcobblers.com/">
-          <span class="flex gap-x-2">
-            <FaBlogSolid font-size={24}/>
-          <s></s></span>
-        </Link>
+      </Link>
     </p>
   )
 }
