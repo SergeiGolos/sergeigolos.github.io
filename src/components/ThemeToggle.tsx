ec5bd41a7a43c1721a20dacd488b116987b2d88a
Sergei Golos <sergei.golos@gmail.com>
2024-01-21 08:25:15 -0500

    enabled dark mode button on the page

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -29,7 +29,16 @@ export function ThemeToggle() {
     <button
       type="button"
       class="group absolute right-4 top-4 z-50 -m-2.5 p-2.5"
-      onClick$={$(() => console.log(otherTheme))}
+      onClick$={$(() => {
+        const theme = document.documentElement.className;
+            if (theme === "light") {
+              document.documentElement.className = "dark";
+              localStorage.setItem("theme", "dark");
+            } else {
+              document.documentElement.className = "light";
+              localStorage.setItem("theme", "light");
+            }
+      })}
     >
       <span class="sr-only">Switch to {otherTheme} theme</span>
       <ThemeIcon class="h-6 w-6 fill-white opacity-50 transition-opacity group-hover:opacity-100 lg:fill-gray-900 lg:dark:fill-white" />
